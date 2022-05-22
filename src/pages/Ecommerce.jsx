5070385296c1863d74a19507ef0d3ff29b190340
tgentreau <gentreau.thomas@gmail.com>
2022-05-22 10:14:50 +0200

    implamating charts

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -11,13 +11,20 @@ import { useStateContext } from '../contexts/contextProvider';
 function Ecommerce() {
   return (
     <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
               <p className="text-2xl">$63,448.78</p>
             </div>
+            <button
+              type="button"
+              style={{ backgroundColor: "blue" }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+            >
+              <BsCurrencyDollar />
+            </button>
           </div>
           <div className="mt-6">
             <Button
@@ -40,7 +47,7 @@ function Ecommerce() {
               </button>
               <p className="mt-3">
                 <span className="text-lg font-semibold">{item.amount}</span>
-                <span className={`text-sm text-${item.pcColor}  ml-2`}>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
               </p>
@@ -49,11 +56,12 @@ function Ecommerce() {
           ))}
         </div>
       </div>
+
       <div className="flex gap-10 flex-wrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
-            <div className="flex justify-between">
-              <p className="font-semibold text-xl">Revenue Updates</p>
-              <div className="flex items-center gap-4">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
                 <span>
                   <GoPrimitiveDot />
@@ -66,10 +74,10 @@ function Ecommerce() {
                 </span>
                 <span>Budget</span>
               </p>
-              </div>
             </div>
-            <div className="mt-10 flex gap-10 flex-wrap justify-center">
-              <div className="border-r-1 border-color m-4 pr-10">
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className=" border-r-1 border-color m-4 pr-10">
               <div>
                 <p>
                   <span className="text-3xl font-semibold">$93,438</span>
@@ -81,10 +89,26 @@ function Ecommerce() {
               </div>
               <div className="mt-8">
                 <p className="text-3xl font-semibold">$48,487</p>
+
                 <p className="text-gray-500 mt-1">Expense</p>
               </div>
+
+              <div className="mt-5">
+                <SparkLine currentColor="blue" id="line-sparkLine" type="Line" height="80px" width="250px" data={SparklineAreaData} color="blue" />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
               </div>
             </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
